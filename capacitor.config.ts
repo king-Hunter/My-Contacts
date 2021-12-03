566ecb127452d620862c59d17b1285c6b8eddfd0
Juan A <esjhuan1000@gmail.com>
2021-12-03 16:53:58 -0600

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'khd.proyects.contacts',
+  appName: 'My Contacts',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
